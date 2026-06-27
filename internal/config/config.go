298dba8f87de0a439de8913bfdd1c3c0607032a5
Abdul Samad Kirmani <abdul.samadkirmani.samad63@gmail.com>
2026-06-27 13:41:43 +0530

    Adding YAML consumer config

---
+++ internal/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+// QueueConfig represents the configuration for a single queue.
+type QueueConfig struct {
+	Name             string `yaml:"name"`
+	MaxSize          int    `yaml:"maxSize"`
+	BackpressureMode string `yaml:"backpressureMode"`
+}
+
+// Config represents the overall application configuration.
+type Config struct {
+	Queues []QueueConfig `yaml:"queues"`
+}
+
+// LoadConfig reads and parses the configuration file at the given path.
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
